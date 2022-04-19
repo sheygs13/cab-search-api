493d59b675fe9848a00b3701609e6e13e4136935
sheygs <segun.ekoh@gmail.com>
2022-04-19 19:19:44 +0100

    chore: Dev configs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Request, Response, NextFunction, Application } from 'express';
 import { urlencoded, json } from 'body-parser';
 import helmet from 'helmet';
 import 'dotenv/config';
+import { Server } from 'http';
 
-const PORT = process.env.PORT || 5000;
-const app: Express = express();
+const PORT: number = Number(process.env.PORT) || 5000;
+const app: Application = express();
 
 app.use(helmet());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
         res.status(200).json({ message: 'OK' });
 });
 
-app.listen(PORT, () => console.log(`server App listening on port ${PORT} ⚡...`));
+const server: Server = app.listen(PORT, () =>
+        console.log(`server App listening on port ${PORT} ⚡...`)
+);
+
+export default server;
